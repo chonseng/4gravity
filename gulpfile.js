ac3a1db76f37975ff509ddf459e605f205e0f123
chonseng <chonseng38@hotmail.com>
2015-07-03 11:52:24 +0800

    Add Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/style.scss')
+        .pipe(sass())
+        .pipe(rename("all.css"))
+        .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src([
+    	'js/source/game.js'])
+        .pipe(uglify())
+            .pipe(concat('script.js'))
+        .pipe(gulp.dest('js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/source/*.js', ['scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['scripts', 'watch']);
